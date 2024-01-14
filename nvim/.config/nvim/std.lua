531d93b3330f8bcdd91561ab7797ee6bdf270f72
Jonathan Rudman <jonathan.rudman@live.co.uk>
2024-01-14 21:31:55 +0000

    Faster pane switching

--- nvim/.config/nvim/std.lua
+++ nvim/.config/nvim/std.lua
@@ -4,3 +4,13 @@ vim.g.mapleader = " "
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 100
+
+-- Disable netrw in favour of nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Shorter pane switching
+vim.keymap.set("n", "<c-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", "<cmd>wincmd l<CR>")
